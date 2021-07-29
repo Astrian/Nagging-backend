907949defa669d319d18dd0069d843767b5b5d81
Astrian Zheng <git@astrianzheng.cn>
2021-07-29 21:24:40 +0800

    Pager of nagging list

--- func/naggings/getList.js
+++ func/naggings/getList.js
@@ -1,8 +1,15 @@
 const util = require("../../util")
 
 module.exports = async (pager) => {
-  let naggingsList = await util.mongodb.read('naggings', {}, {
-    sort: { time: -1 }
+  let limit = 20
+  if (!pager) pager = 0
+  let list = await util.mongodb.read('naggings', {}, {
+    sort: { time: -1 },
+    limit,
+    skip: limit*pager
   })
-  return naggingsList
+  let next = false
+  let res = await util.mongodb.read('naggings', {}, { sort: { time: -1 }, limit: 1, skip: (limit*(pager+1)) })
+  if (res.length) next = true
+  return {list, next}
 }
