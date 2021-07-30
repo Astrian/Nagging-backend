6b59b5d38c5124c9c1aa5cf3bdeccab6d5b22925
Astrian Zheng <git@astrianzheng.cn>
2021-07-30 10:49:26 +0800

    fetch avatar

--- func/users/getUsers.js
+++ func/users/getUsers.js
@@ -4,6 +4,6 @@ const { ApolloError } = require('apollo-server-errors')
 module.exports = async () => {
   let res = await util.mongodb.read("users", {})
   if (!res.length) throw new Error(`Cannot find the user`, `RESOURCE_NOT_FOUND`)
-  let {username, fullname, uuid, bio} = res[0]
-  return { username, fullname, uuid, bio}
+  let {username, fullname, uuid, bio, avatar} = res[0]
+  return { username, fullname, uuid, bio, avatar}
 }
