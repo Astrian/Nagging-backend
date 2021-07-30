4fd080a21e2e30063a0e12c13cf19de8237c8a75
Astrian Zheng <git@astrianzheng.cn>
2021-07-30 10:36:10 +0800

    clear console.log

--- func/users/editProfile.js
+++ func/users/editProfile.js
@@ -1,7 +1,6 @@
 const util = require("../../util")
 
 module.exports = async (args, uuid) => {
-  console.log(args)
   if (args.username) await util.mongodb.edit('users', {uuid}, {username: args.username})
   if (args.bio) await util.mongodb.edit('users', {uuid}, {bio: args.bio})
   if (args.fullname) await util.mongodb.edit('users', {uuid}, {fullname: args.fullname})
